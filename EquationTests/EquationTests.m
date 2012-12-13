7240a520f4cbc3d914ff931ea50f6f765669228f
ookubo yuma <r0531011@eng.kitakyu-u.ac.jp>
2012-12-13 16:25:08 +0900

    課題２

--- EquationTests/EquationTests.m
+++ EquationTests/EquationTests.m
@@ -127,4 +127,57 @@ Equation* e;
 // 実は虚数や0で割り算した場合など，計算結果が実数で表せない値になってしまったとき
 // nan という特殊な数値になります。
 // それでテストがパスしてしまったのですね。
+
+//課題２
+// x^2 - x - 20 = 0 をテスト
+// 実数解 2つの場合。
+- (void)test5
+{
+    e = [[Equation alloc] initWithA:1 b:-1 c:-20];
+    
+    // STAssertEqualsWithAccuracy は浮動小数点演算をテストするときに使います。
+    // 浮動小数点演算では誤差が発生するので，誤差の範囲内で等しいことを確かめる必要があります。
+    // accuracy とは誤差のことです。
+    STAssertEqualsWithAccuracy(5.0, [e real1], 0.00001, @"real1 error");
+    STAssertEqualsWithAccuracy(-4.0, [e real2], 0.00001, @"real2 error");
+    STAssertEqualsWithAccuracy(0.0, [e imaginary1], 0.00001, @"imaginary1 error");
+    STAssertEqualsWithAccuracy(0.0, [e imaginary2], 0.00001, @"imaginary2 error");
+}
+
+// x^2 -6x + 9 = 0 をテスト
+// 実数解1つ(重解)の場合
+- (void)test6
+{
+    e = [[Equation alloc] initWithA:1 b:-6 c:9];
+    
+    STAssertEqualsWithAccuracy(3.0, [e real1], 0.00001, @"real1 error");
+    STAssertEqualsWithAccuracy(3.0, [e real2], 0.00001, @"real2 error");
+    STAssertEqualsWithAccuracy(0.0, [e imaginary1], 0.00001, @"imaginary1 error");
+    STAssertEqualsWithAccuracy(0.0, [e imaginary2], 0.00001, @"imaginary2 error");
+}
+
+// test1, test2 について。
+//
+// このように異なる例を与えて動作を確かめます。
+// テストで与える例のことをテストケースといいます。
+
+// x^2 + 2x + 10 = 0 をテスト
+// 虚数解 2つの場合。
+- (void)test7
+{
+    e = [[Equation alloc] initWithA:1 b:2 c:10];
+    
+    STAssertEqualsWithAccuracy(-1.0, [e real1], 0.00001, @"real1 error");
+    STAssertEqualsWithAccuracy(-1.0, [e real2], 0.00001, @"real2 error");
+    STAssertEqualsWithAccuracy(3.0, [e imaginary1], 0.00001, @"imaginary1 error");
+    STAssertEqualsWithAccuracy(-3.0, [e imaginary2], 0.00001, @"imaginary2 error");
+}
+
+// ソフトウェアテストは同値クラスではないものを網羅的に行ったほうがいいです。
+// 2次方程式の解の場合，実数解2つ，重解と来たので，次は虚数解2つでしょうね。
+// 虚数解2つの場合として test4 を作ってみました。どうでしょう？ パスしましたか？
+// 本当にこれでよかったんですかね？
+//
+// 試しにわざとエラーになるようにしてみましょう。
+// 同じ式で期待値をわざと変えてみました。
 @end
